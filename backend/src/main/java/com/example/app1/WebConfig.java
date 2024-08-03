215e5418065a5ad17fd795f2b4d1348949808a7d
Kamil Ayupov <ayupovkamil@gmail.com>
2024-08-03 13:26:21 +0300

    Fix: path pattern

--- backend/src/main/java/com/example/app1/WebConfig.java
+++ backend/src/main/java/com/example/app1/WebConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
+        registry.addMapping("/api/**")
                 .allowedOrigins("https://robotick.ru") // Разрешенные домены
                 .allowedMethods("GET", "POST") // Разрешенные методы
                 .allowedHeaders("Authorization", "Content-Type") // Разрешенные заголовки
