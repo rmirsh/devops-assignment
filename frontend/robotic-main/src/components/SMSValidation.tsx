0b4a5df88858a8d75def82afe4f8f4640a8736f2
Kamil Ayupov <ayupovkamil@gmail.com>
2024-08-03 13:30:22 +0300

    Fix: backend url

--- frontend/robotic-main/src/components/SMSValidation.tsx
+++ frontend/robotic-main/src/components/SMSValidation.tsx
@@ -75,7 +75,7 @@ const SMSValidation: React.FC<SMSProps> = ({onClose, data}) => {
 
     const passwordValidation = () => {
         const code = (smsContent1 + smsContent2 + smsContent3 + smsContent4).trim();
-        const url = 'https://robotick.ru/api/submitUserData';
+        const url = 'https://backend:8000/submitUserData';
         console.log(JSON.stringify({...data, code: code}) + '  отправляемые данные с кодом из смс')
         return fetch(url, {
             method: "POST",
@@ -117,7 +117,7 @@ const SMSValidation: React.FC<SMSProps> = ({onClose, data}) => {
     }
 
     const passwordValidationAgain = () => {
-        const url = 'https://robotick.ru/api/isPhoneValid';
+        const url = 'https://backend:8000/isPhoneValid';
         return fetch(url, {
             method: "POST",
             body: JSON.stringify({phone: data.phone}),
